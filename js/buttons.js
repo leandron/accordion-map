52521a61fc498cc8ed2d0dc3a60304ef9814d8b0
Leandro Nunes <lnunix@gmail.com>
2013-05-26 21:27:02 -0300

    first version with chords.

--- js/buttons.js
+++ js/buttons.js
@@ -23,46 +23,95 @@ HOHNER_GC = [HOHNER_GC_OPEN, HOHNER_GC_CLOSE]
 OPEN = 0;
 CLOSE = 1;
 
+c_variations = 2;
+
 /* chords */
 
 /* [chord_name, 'complement', OPEN/CLOSE, [[row,key],[row,key]...], 'obs'] */
 
 chords = [
     [C, '', [
-        [CLOSE, [[1,2],[1,3],[1,4]],''],
-        [CLOSE, [[1,5],[1,6],[1,7]],''],
-        [OPEN, [[2,3],[2,4],[1,4]],''],
-        [OPEN, [[2,7],[2,8],[1,8]],''],
+        [CLOSE, [[0,2],[0,3],[0,4]],''],
+        [CLOSE, [[0,5],[0,6],[0,7]],''],
+        [OPEN, [[1,3],[1,4],[0,4]],''],
+        [OPEN, [[1,7],[1,8],[0,8]],''],
         ]
     ],
     [D, '', [
-        [OPEN, [[2,0],[2,1],[2,2]],''],
-        [OPEN, [[1,2],[2,6],[2,7]],''],
-        [OPEN, [[1,6],[2,9],[2,10]],''],
+        [OPEN, [[1,0],[1,1],[1,2]],''],
+        [OPEN, [[0,2],[1,5],[1,6]],''],
+        [OPEN, [[0,6],[1,9],[1,10]],''],
         ]
     ],
     [D, 'm', [
-        [OPEN, [[1,2],[1,3],[2,6]],''],
-        [OPEN, [[1,6],[1,7],[2,10]],''],
+        [OPEN, [[0,2],[0,3],[1,6]],''],
+        [OPEN, [[0,6],[0,7],[1,10]],''],
         ]
     ],
     [E, '7', [
-        [CLOSE, [[2,3],[1,3],[2,4]],''],
-        [CLOSE, [[2,6],[1,6],[2,7]],''],
-        [CLOSE, [[2,9],[1,9],[2,10]],''],
+        [CLOSE, [[0,3],[1,3],[1,4]],''],
+        [CLOSE, [[0,6],[1,6],[1,7]],''],
+        [CLOSE, [[0,9],[1,9],[1,10]],''],
+        ]
+    ],
+    [E, 'm', [
+        [CLOSE, [[0,3],[0,4],[1,6]],''],
+        [OPEN,  [[1,4],[0,4],[0,5]],''],
+        [CLOSE, [[0,6],[0,7],[1,8]],''],
+        [OPEN,  [[1,8],[0,8],[0,9]],''],
+        ]
+    ],
+    [F, '', [
+        [OPEN,  [[1,2],[1,3],[0,3]],''],
+        [OPEN,  [[1,6],[1,7],[0,7]],''],
+        ]
+    ],
+    [G, '', [
+        [OPEN,  [[0,0],[0,1],[0,2]],''],
+        [CLOSE, [[1,2],[1,3],[1,4]],''],
+        [OPEN,  [[0,4],[0,5],[0,6]],''],
+        [CLOSE, [[1,5],[1,6],[1,7]],''],
+        [CLOSE, [[1,8],[1,9],[1,10]],''],
+        ]
+    ],
+    [A, 'm', [
+        [OPEN,  [[1,2],[1,3],[1,4]],''],
+        [OPEN,  [[1,6],[1,7],[1,8]],''],
         ]
     ],
 ];
 
+function clear_chords () {
+    for (i = 0; i < HOHNER_GC_OPEN[0].length; i++) {
+        $("#row_0_btn_" + i).removeClass('acc_highlight_up');
+        $("#row_0_btn_" + i).removeClass('acc_highlight_down');
+    }
+
+    for (i = 0; i < HOHNER_GC_OPEN[1].length; i++) {
+        $("#row_1_btn_" + i).removeClass('acc_highlight_up');
+        $("#row_1_btn_" + i).removeClass('acc_highlight_down');
+    }
+
+}
+
 function set_chord(chord_no, var_no) {
-    console.log("chord " + chord_no);
-    console.log("var " + var_no);
+    clear_chords();
+    note_list = chords[chord_no][c_variations][var_no][1];
+
+    if (chords[chord_no][c_variations][var_no][0] == CLOSE) {
+        func_mark = mark_button_up;
+    } else {
+        func_mark = mark_button_down;
+    }
+
+    for (i=0; i < note_list.length; i++) {
+        func_mark(note_list[i][0], note_list[i][1]);
+    }
 }
 
 function create_chord_items(my_div) {
     var c_symbol = 0;
     var c_complement = 1;
-    var c_variations = 2;
 
     // high level chords
     for (var c=0; c < chords.length; c++) {
@@ -84,7 +133,7 @@ function create_chord_items(my_div) {
 
 
 function fill_row(my_div, max_buttons) {
-    for (var i=1; i <= max_buttons; i++) {
+    for (var i=0; i < max_buttons; i++) {
         up_btn = "<p id=\"" + my_div + "_btn_" + i + "_up\" class=\"acc_label_up\"> A </p>";
         down_btn = "<p id=\"" + my_div + "_btn_" + i + "_down\"  class=\"acc_label_down\"> B </p>";
         div_str = "<div class=\"acc_button\" id=\"" + my_div + "_btn_" + i + "\"> " + up_btn + down_btn + " </div>";
@@ -94,6 +143,14 @@ function fill_row(my_div, max_buttons) {
 
 function get_button_obj (row, button_number) {
     return $("#row_" + row + "_btn_" + button);
+}
+
+function mark_button_up (row, button) {
+    $("#row_" + row + "_btn_" + button).addClass('acc_highlight_up');
+}
+
+function mark_button_down (row, button) {
+    $("#row_" + row + "_btn_" + button).addClass('acc_highlight_down');
 }
 
 function set_button_label_up (row, button, label) {
@@ -113,17 +170,17 @@ function set_tone(tones, tone_adjust, tone_labels) {
         bottom_label_idx = (tones[OPEN][up_row][i] + tone_adjust) % int_tone.length;
         up_label_idx = (tones[CLOSE][up_row][i] + tone_adjust) % int_tone.length;
         
-        set_button_label_up(1, (i+1), tone_labels[up_label_idx]);
-        set_button_label_down(1, (i+1), tone_labels[bottom_label_idx]);
+        set_button_label_up(0, i, tone_labels[up_label_idx]);
+        set_button_label_down(0, i, tone_labels[bottom_label_idx]);
     }
 
     bottom_row_len = tones[OPEN][bottom_row].length;
     for(var i=0; i < bottom_row_len; i++) {
-        up_label_idx = (tones[OPEN][bottom_row][i] + tone_adjust) % int_tone.length;
-        bottom_label_idx = (tones[CLOSE][bottom_row][i] + tone_adjust) % int_tone.length;
+        bottom_label_idx = (tones[OPEN][bottom_row][i] + tone_adjust) % int_tone.length;
+        up_label_idx = (tones[CLOSE][bottom_row][i] + tone_adjust) % int_tone.length;
         
-        set_button_label_up(2, (i+1), tone_labels[up_label_idx]);
-        set_button_label_down(2, (i+1), tone_labels[bottom_label_idx]);
+        set_button_label_up(1, i, tone_labels[up_label_idx]);
+        set_button_label_down(1, i, tone_labels[bottom_label_idx]);
     }
 
 }
